554fe08af866583f00b70420398dedd230f2e6bd
Jonathan <jonathan.bosman@gmail.com>
2024-11-12 14:05:02 +0100

    localhost 5173 en cors origin

--- back/server.js
+++ back/server.js
@@ -15,7 +15,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:8080'
+    origin: 'http://localhost:5173'
 }))
 const swaggerOptions = {
     swaggerDefinition: {
